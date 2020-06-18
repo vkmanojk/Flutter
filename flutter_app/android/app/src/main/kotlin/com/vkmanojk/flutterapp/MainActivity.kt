89e854f3ad7f5f1c7bea69869f9849e5832a049e
Manojkumar <vkmanojk@gmail.com>
2020-06-18 21:13:50 +0530

    First flutter app

---
+++ flutter_app/android/app/src/main/kotlin/com/vkmanojk/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vkmanojk.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/android/app/src/main/kotlin/com/vkmanojk/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vkmanojk.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
